30c9ae2ff4168cabf1365ba4fb5abd63a8b6c208
Shane Price <shane.tas.price@gmail.com>
2016-11-23 14:27:21 -0700

    added user model

--- identity-service/server.js
+++ identity-service/server.js
@@ -1,4 +1,13 @@
 
-
 var express = require('express');
 var morgan = require('morgan');
+var config = require('config');
+
+var app = express();
+var port = config.get('server.port');
+
+app.use(morgan('dev'));
+
+app.listen(port, function() {
+	console.log('listening on port ' + port);
+});
