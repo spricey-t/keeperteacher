05889f4bc42c7df6085239b012567f54e6e57c1c
Shane Price <shane.tas.price@gmail.com>
2016-11-26 10:30:34 -0700

    working on dart angular 2 app

--- admin-ui/app/server.js
+++ admin-ui/app/server.js
@@ -8,7 +8,8 @@ const app = express();
 const port = config.get('server.port');
 
 
-app.use(express.static('client'));
+app.use(express.static('client/admin_ui/build/web/'));
+app.use('/lib', express.static('client/lib/'));
 
 app.listen(port, () => {
 	winston.info('server started on port', port);
