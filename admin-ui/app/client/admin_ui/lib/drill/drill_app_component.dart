6200c7726b0f5ff81d412cb08a6dc6cfeca61d5a
Shane Price <shane.tas.price@gmail.com>
2016-12-19 11:35:38 -0700

    moved drill loading to app component

--- admin-ui/app/client/admin_ui/lib/drill/drill_app_component.dart
+++ admin-ui/app/client/admin_ui/lib/drill/drill_app_component.dart
@@ -3,18 +3,39 @@ import 'package:angular2/core.dart';
 
 import 'package:admin_ui/drill/list/drill_list_component.dart';
 import 'package:admin_ui/drill/drill.dart';
+import 'package:admin_ui/drill/drill_service.dart';
+import 'package:admin_ui/drill/drill_search_service.dart';
 
 @Component(
     selector: 'kt-drill-app',
     templateUrl: 'drill_app_component.html',
     styleUrls: const ['drill_app_component.css'],
-    directives: const [DrillListComponent]
+    directives: const [DrillListComponent],
+    providers: const [DrillService, DrillSearchService]
 )
-class DrillAppComponent {
+class DrillAppComponent implements OnInit {
 
-  Drill selectedDrill;
+  final DrillService _drillService;
+  final DrillSearchService _drillSearchService;
 
-  void onDrillListNotify(Drill drill) {
-    selectedDrill = drill;
+  final List<Drill> drills;
+  final List<Drill> filteredDrills;
+
+  DrillAppComponent(this._drillService, this._drillSearchService)
+      : drills = new List(), filteredDrills = new List() {
+  }
+
+  @override
+  ngOnInit() {
+    _loadDrills();
+  }
+
+  void _loadDrills() {
+    drills.clear();
+    _drillService.listDrills().then((loadedDrills) {
+      drills.addAll(loadedDrills);
+      filteredDrills.clear();
+      filteredDrills.addAll(drills);
+    });
   }
 }
